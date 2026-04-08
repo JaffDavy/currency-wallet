8e55ec72fe3085c13bb6efe8879f525125ae4f46
JaffDavy <jaffdavy100@gmail.com>
2026-04-08 15:28:16 +0100

    chore: initial vite + tailwind setup

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+     <h1 className="text-3xl font-black tracking-tighter text-slate-900">
+        Sanchos-Currency<span className="text-orange-500">Wallet.</span>
+      </h1>
+    </>
+  )
+}
+
+export default App
